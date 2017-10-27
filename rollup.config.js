ab39d95341cd4c85326569af2618ff233fbd8615
Kasper Pihl Tornøe <kasper@pihl.it>
2017-10-27 15:11:49 +0300

    changing rollup to style from react router

--- rollup.config.js
+++ rollup.config.js
@@ -7,75 +7,31 @@ import { minify } from 'uglify-es';
 import pkg from './package.json';
 
 const prod = process.env.NODE_ENV === 'production';
-const config = [
-  // browser-friendly UMD build
-  {
-    input: './src/index',
-    output: {
-      file: pkg.browser,
-      format: 'umd',
-    },
-    external: [ 'react' ],
-    globals: {
-      react: 'React'
-    },
-    name: 'Swiss',
-    plugins: [
 
-      babel({
-        exclude: ['node_modules/**']
-      }),
-      prod && replace({
-        'process.env.NODE_ENV': JSON.stringify(prod ? 'production' : 'development'),
-      }),
-      resolve(), // so Rollup can find `ms`
-      commonjs({
-        ignoreGlobal: true,
-      }),
-      prod && uglify({}, minify)
-    ]
+const config = {
+  input: './src/index',
+  output: [
+    { file: pkg.main, format: 'umd' },
+    { file: pkg.module, format: 'es' }
+  ],
+  external: [ 'react' ],
+  globals: {
+    react: 'React'
   },
-
-  // CommonJS (for Node) and ES module (for bundlers) build.
-  // (We could have three entries in the configuration array
-  // instead of two, but it's quicker to generate multiple
-  // builds from a single configuration where possible, using
-  // the `targets` option which can specify `dest` and `format`)
-  {
-    input: 'src/index.js',
-    external: [ 'react' ],
-    output: [
-      { file: pkg.main, format: 'cjs' },
-      { file: pkg.module, format: 'es' }
-    ],
-    globals: {
-      react: 'React'
-    },
-    name: 'Swiss',
-    plugins: [
-      babel({
-        exclude: ['node_modules/**']
-      }),
-      prod && replace({
-        'process.env.NODE_ENV': JSON.stringify(prod ? 'production' : 'development'),
-      }),
-      resolve(),
-      prod && uglify({}, minify)
-    ]
-  }
-];
-
-// {
-//   plugins: [
-//     resolve(),
-//     replace({
-//       'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-//     })
-//   ]
-// }
-
-// if (process.env.NODE_ENV === 'production') {
-//   config.plugins.push(uglify())
-// }
+  name: 'Swiss',
+  plugins: [
+    babel({
+      exclude: ['node_modules/**']
+    }),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify(prod ? 'production' : 'development'),
+    }),
+    resolve(), // so Rollup can find `ms`
+    commonjs({
+      ignoreGlobal: true,
+    }),
+    prod && uglify({}, minify)
+  ]
+};
 
 export default config;
