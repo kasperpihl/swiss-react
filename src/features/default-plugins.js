ec95fcb1e48114763920863e127d46b98f7b3105
thinklinux <tihomir.valkanov@gmail.com>
2019-04-10 12:43:40 +0300

    Removing the plugin for converting strings to numbers when in react-native env. This fixes using enums like '100', '200' etc. for styling in RN.

--- src/features/default-plugins.js
+++ src/features/default-plugins.js
@@ -1,22 +1,8 @@
 import { addPlugin } from './plugins';
 
-if (typeof navigator != 'undefined' && navigator.product == 'ReactNative') {
-  // I'm in react-native
-  // Parse '9' to 9. React native uses numbers and not strings for styles.
-  addPlugin('parseKeyValue', (k, v) => {
-    const intRegex = /^([+-]?[1-9]\d*|0)$/;
-    const floatRegex = /^[-+]?[0-9]*\.?[0-9]+([eE][-+]?[0-9]+)?$/;
-    if (intRegex.test(v)) {
-      return [k, parseInt(v, 10)];
-    } else if (floatRegex.test(v)) {
-      return [k, parseFloat(v)];
-    }
-  });
-} else {
-  // I'm on the web or node js!
-  // CSS:after content fix. Turns 'value' into ''value'';
-  addPlugin(
-    'parseKeyValue',
-    (k, v) => k === 'content' && v === '' && [k, `'${v}'`]
-  );
-}
+// I'm on the web or node js!
+// CSS:after content fix. Turns 'value' into ''value'';
+addPlugin(
+  'parseKeyValue',
+  (k, v) => k === 'content' && v === '' && [k, `'${v}'`]
+);
