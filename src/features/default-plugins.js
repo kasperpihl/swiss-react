693561be182fe4dffacec6b65453601302292eb0
thinklinux <tihomir.valkanov@gmail.com>
2019-04-12 09:10:00 +0300

    adding a condition for RN

--- src/features/default-plugins.js
+++ src/features/default-plugins.js
@@ -1,7 +1,9 @@
 import { addPlugin } from './plugins';
 
-// CSS:after content fix. Turns 'value' into ''value'';
-addPlugin(
-  'parseKeyValue',
-  (k, v) => k === 'content' && v === '' && [k, `'${v}'`]
-);
+if (navigator.product != 'ReactNative') {
+  // CSS:after content fix. Turns 'value' into ''value'';
+  addPlugin(
+    'parseKeyValue',
+    (k, v) => k === 'content' && v === '' && [k, `'${v}'`]
+  );
+}
